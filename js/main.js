cce54297162c3d373669e4d3b0fc16c05d06d8c7
R-Tsukada <r.tsukada14@gmail.com>
2021-11-02 12:20:46 +0900

    ローカルストレージにデータを保存する機能を追加

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
+const STORAGE_KEY = "todos-vuejs";
+const todoStorage = {
+  fetch() {
+    const todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]");
+    todos.forEach((todo, index) => {
+      todo.id = index;
+    });
+    todoStorage.uid = todos.length;
+    return todos;
+  },
+  save(todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
+  }
+};
+
 const app = Vue.createApp({
   data:() => ({
     newTodo: '',
-    todos: []
+    todos: todoStorage.fetch(),
   }),
   methods: {
     addTask: function(event) {
@@ -10,6 +25,14 @@ const app = Vue.createApp({
         task: this.newTodo,
       }
       this.todos.push(todo)
+    }
+  },
+  watch: {
+    todos: {
+      handler: function(todos) {
+        todoStorage.save(todos)
+      },
+      deep: true
     }
   }
 })
