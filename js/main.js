a999e5c637e1b3044d38fc690981a88bb726d477
R-Tsukada <r.tsukada14@gmail.com>
2021-11-02 11:58:31 +0900

    新規タスク入力の保存と、jsディレクトリへのパスを修正

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
 const app = Vue.createApp({
-
+  data:() => ({
+    newTodo: '',
+    todos: []
+  }),
+  methods: {
+    addTask: function(event) {
+      console.log('success')
+      let todo = {
+        task: this.newTodo,
+      }
+      this.todos.push(todo)
+    }
+  }
 })
 app.mount('#app')
